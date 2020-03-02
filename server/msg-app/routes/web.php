3880851da47708cef0e419c5017e8180776aa489
Yuina-Kage <yuina.kg@gmail.com>
2020-03-02 15:49:09 +0900

    メッセージアプリ2

--- server/msg-app/routes/web.php
+++ server/msg-app/routes/web.php
@@ -14,5 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('message/{msg}', 'MessageController@show');
+Route::get('message/{name}/{msg}', 'MessageController@show');
 
